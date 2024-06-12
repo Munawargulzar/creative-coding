337068d1773a6f75058becf339cc15c31140a0ec
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-06-12 17:08:21 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,112 +1,27 @@
-let board = [
-  
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
-];
-let currentPlayer = 'X';
 
+var numFrames = 6 ; //total images/total frames
+var frame = 0 // always 0
+var images = new Array(numFrames);
+//Array - stores multiple data/images
+
+function preload(){
+      images[0] = loadImage("rabit 1.png");
+      images[1] = loadImage("rabit 2.png");
+      images[2] = loadImage("rabit 3.png");
+      images[3] = loadImage("rabit 4.png");
+      images[4] = loadImage("rabit 5.png");
+      images[5] = loadImage("rabit 6.png");
+      
+ }
 function setup() {
-  createCanvas(250, 250);
-  drawBoard();
+  createCanvas(600, 400);
+  frameRate(20); //speed of the flying bird
+  background(0);
 }
-  
+
 function draw() {
-}
-
-function drawBoard() {
-  background(255);
-  let w = width / 3;
-  let h = height / 3;
-  strokeWeight(4);
-  for (let i = 0; i < 3; i++) {
-    line(i * w, 0, i * w, height);
-    line(0, i * h, width, i * h);
-  }
-
-  for (let j = 0; j < 3; j++) {
-    for (let i = 0; i < 3; i++) {
-      let x = i * w + w / 2;
-      let y = j * h + h / 2;
-      let spot = board[i][j];
-      textSize(32);
-      let r = w / 4;
-      if (spot == 'X') {
-        line(x - r, y - r, x + r, y + r);
-        line(x + r, y - r, x - r, y + r);
-      } else if (spot == 'O') {
-        noFill();
-        ellipse(x, y, w / 2);
-      }
-    }
-  }
-}
-
-function mousePressed() {
-  let i = floor(mouseX / (width / 3));
-  let j = floor(mouseY / (height / 3));
-  if (board[i][j] == '') {
-    board[i][j] = currentPlayer;
-    if (checkWinner(currentPlayer, i, j)) {
-      console.log(currentPlayer + ' wins!');
-      noLoop();
-    } else if (checkDraw()) {
-      console.log('Draw!');
-      noLoop();
-    } else {
-      currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
-    }
-  }
-  drawBoard();
-}
-
-function checkWinner(player, x, y) {
-  for (let i = 0; i < 3; i++) {
-    if (board[i][y] != player) {
-      break;
-    }
-    if (i == 2) {
-      return true;
-    }
-  }
-  for (let i = 0; i < 3; i++) {
-    if (board[x][i] != player) {
-      break;
-    }
-    if (i == 2) {
-      return true;
-    }
-  }
-  if (x == y) {
-    for (let i = 0; i < 3; i++) {
-      if (board[i][i] != player) {
-        break;
-      }
-      if (i == 2) {
-        return true;
-      }
-    }
-  }
-  if (x + y == 2) {
-    for (let i = 0; i < 3; i++) {
-      if (board[i][2 - i] != player) {
-        break;
-      }
-      if (i == 2) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-function checkDraw() {
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (board[i][j] == '') {
-        return false;
-      }
-    }
-  }
-  return true;
+  background(225);
+  frame++; //increment - add 1 value
+  if (frame == numFrames) frame = 0; //if 0 will be equal to 6, then it will go back again to 0
+  image(images[frame], mouseX - 75, mouseY - 100); // mouse interaction
 }
