23a2053aa54b58ced223a4ca057cdb37f608f273
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-06-12 21:19:18 +0400

    Add files via upload

---
+++ 5._typography_2024_06_12_16_45_48/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 200);
+  background(220);
+  
+  textSize(36);
+  textAlign(CENTER, TOP);
+  textStyle(BOLDITALIC);
+  fill(0);
+  text('Hello', width/2, 20);
+  
+  textSize(24);
+  textStyle(NORMAL);
+  text('my name is munawar', width/2, height/2);
+  
+  textSize(20);
+  text('i want to be a python developer', width/2, height/2 + 40);
+}
