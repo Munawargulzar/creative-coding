b9f9b36a92904f309758d022fe9ed1bfd53c2880
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-06-12 17:46:58 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,12 @@
-let song;
-let fft;
-
-function preload() {
-  song = loadSound('song.mp3');
-}
-
 function setup() {
   createCanvas(400, 400);
-  song.play();
-  fft = new p5.FFT();
+  background(220);
 }
 
 function draw() {
-  background(0);
-
-  let spectrum = fft.analyze();
-  noStroke();
-  fill(255);
-
-  for (let i = 0; i < spectrum.length; i++) {
-    let x = map(i, 0, spectrum.length, 0, width);
-    let h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h);
-  }
+  fill(220, 20);
+  rect(0, 0, width, height);
+  
+  fill(255, 0, 0);
+  ellipse(mouseX, mouseY, 50, 50);
 }
