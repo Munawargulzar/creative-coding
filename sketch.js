cd59abac807b0698633762c3332a8743b90739a3
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-06-12 17:28:36 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,16 @@
-var img, x,y ;//declaring the variables
-function preload()
-{
-img = loadImage("green tree.jpg");
-}
+var img;
+function preload() {
+img = loadImage("nature.jpg"); /// preload the image to make sure that its loaded before setup
+  }
 
 function setup() {
   createCanvas(400, 400);
-  background(0);
-  noStroke();
+  background(0);// set the background to black
 }
 
 function draw() {
- 
- 
-  x = random(width); // randomly place a dot in x position
-  y = random(height); // randomly place a dot in y position
- var c = img.get(x,y); // getting the position of the mouse
-  fill(c[0], c[1], c[2], 100); //the color depends on the dots , 50 transparency value
-  triangle(x, y - 30,x - 30,y + 30,x + 30, y  + 30);
+  background(0);
+  image(img,0,0);
+  var v = map(mouseX, 0, width, 0 , 70);// mark the map postion mouseX between 0 to 70 for the posterize effect
+  filter(POSTERIZE, v);
 }
