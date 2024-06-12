96c26a83bbb76c9bc60f34208f3818e11dfecf33
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-06-12 18:01:13 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,31 @@
-let alienX = 200;
-let alienY = 200;
-
 function setup() {
   createCanvas(400, 400);
-}
-
-function draw() {
-  background(0);
-
-  fill(150, 200, 255); 
-  ellipse(alienX, alienY, 100, 150); 
+  background(220);
   
-  fill(100, 150, 200); 
-  rect(alienX - 40, alienY - 50, 30, 100, 20); 
-  rect(alienX + 10, alienY - 50, 30, 100, 20);
-  
-  fill(100, 150, 200); 
-  rect(alienX - 30, alienY + 40, 20, 80, 20); 
-  rect(alienX + 10, alienY + 40, 20, 80, 20);
-  
-  
-  fill(150, 200, 255); 
-  ellipse(alienX, alienY - 90, 80, 80); 
-  
-  fill(0); 
-  ellipse(alienX - 20, alienY - 100, 20, 20); 
-  ellipse(alienX + 20, alienY - 100, 20, 20); 
-  
-  fill(255); 
-  ellipse(alienX - 20, alienY - 100, 10, 10); 
-  ellipse(alienX + 20, alienY - 100, 10, 10); 
-  
-  noFill(); 
-  stroke(255); 
-  arc(alienX, alienY - 80, 40, 20, 0, PI);
+  for (let i = 0; i < 100; i++) {
+    let shape = random(3);
+    
+    let x = random(width);
+    let y = random(height);
+    
+    let s = random(10, 50);
+    
+    let r = random(255);
+    let g = random(255);
+    let b = random(255);
+    
+    fill(r, g, b);
+    noStroke();
+    if (shape < 1) {
+      ellipse(x, y, s, s);
+    } else if (shape < 2) {
+      rect(x, y, s, s);
+    } else {
+      let x2 = x + random(-s, s);
+      let y2 = y + random(-s, s);
+      let x3 = x + random(-s, s);
+      let y3 = y + random(-s, s);
+      triangle(x, y, x2, y2, x3, y3);
+    }
+  }
 }
