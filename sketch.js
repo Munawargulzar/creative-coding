c601bac837069c1d273ba32def55774728b8535f
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-06-12 17:50:00 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 200);
   background(220);
-}
-
-function draw() {
-  fill(220, 20);
-  rect(0, 0, width, height);
   
-  fill(255, 0, 0);
-  ellipse(mouseX, mouseY, 50, 50);
+  textSize(36);
+  textAlign(CENTER, TOP);
+  textStyle(BOLDITALIC);
+  fill(0);
+  text('Hello', width/2, 20);
+  
+  textSize(24);
+  textStyle(NORMAL);
+  text('my name is munawar', width/2, height/2);
+  
+  textSize(20);
+  text('i want to be a python developer', width/2, height/2 + 40);
 }
